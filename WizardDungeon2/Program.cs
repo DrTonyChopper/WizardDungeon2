5a958e348a3b36c5e18b71894d837e63672e52d6
DrTonyChopper <remo.orlandini@virgilio.it>
2015-12-04 16:38:43 +0100

    add new program.cs

--- WizardDungeon2/Program.cs
+++ WizardDungeon2/Program.cs
@@ -1,14 +1,57 @@
 ﻿using System;
 
-namespace WizardDungeon2
+namespace WizardDungeon
 {
-	class MainClass
+  class MainClass
+   {
+	public static void Main(string[] args)
 	{
-		public static void Main (string[] args)
+		Hero mage	= new Hero ();
+		Hero enemy = new Hero ();
+		Dungeon dungeon = new Dungeon (); 
+
+
+		//enemy's coordinates
+			enemy.SetX(14);
+			enemy.SetY(5);
+			enemy.Setlives (10);
+		//hero's coordinates
+			mage.SetX(dungeon.GetSaveX());
+			mage.SetY(dungeon.GetSaveY());
+
+			//mage.inventory [0].setNewItem (1);
+			//mage.inventory [1].setNewItem (2);
+			//mage.inventory [2].setNewItem (3);
+
+
+
+		bool isGameRunning = true;
+		while (isGameRunning)
 		{
-			Console.WriteLine ("Hello World!");
-			Console.Clear ();
-			// hello
+			dungeon.ClearScreen();
+			//print the player's lives
+			mage.PrintInventory();
+			mage.PrintLives(mage, "\u2665");
+			Console.WriteLine();
+			enemy.PrintLives(enemy, "\u2620");
+			Console.WriteLine();	
+			Console.BackgroundColor = ConsoleColor.Yellow;
+			Console.ForegroundColor = ConsoleColor.Black;
+			//update and redraw
+			dungeon.Print(mage, enemy);
+			//input
+			int oldHeroX = mage.GetX();
+			int oldHeroY = mage.GetY();
+			//controls for move the player
+				mage.Move(enemy);
+			//if the Player is over the Enemy
+				dungeon.Test(mage, enemy, ref oldHeroX, ref oldHeroY, ref isGameRunning);
 		}
+
+			if (mage.Getlives() > 0)
+				dungeon.GameOver (true);
+			else
+				dungeon.GameOver (false);
+      }
 	}
 }
